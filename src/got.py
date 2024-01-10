4363071305bcab8fb62bd21162d6432e27683d62
Miguel Ferreira Cordero <72079312+ferreiracorderomiguel@users.noreply.github.com>
2024-01-10 12:03:41 +0100

    Implementado test_reyes_mayor_menor_ejercito

--- src/got.py
+++ src/got.py
@@ -1,5 +1,6 @@
+from collections import defaultdict
 import csv
-from typing import List, NamedTuple, Optional
+from typing import List, NamedTuple, Optional, Tuple
 from parsers import *
 
 
@@ -9,6 +10,13 @@ BatallaGOT = NamedTuple('BatallaGOT',
      ('region', str), ('num_atacantes', Optional[int]), ('num_atacados', Optional[int])])
 
 def lee_batallas(ruta_fichero: str) -> List[BatallaGOT]:
+    '''
+    Lee un fichero de entrada en formato CSV codificado en utf-8 y devuelve una
+    lista de tuplas de tipo BatallaGOT conteniendo todos los datos almacenados
+    en el fichero. Use el método de cadenas split para separar los comandantes
+    atacantes y atacados, y el método de cadenas strip para eliminar los
+    espacios al inicio o final de los nombres de los comandantes. (1,25 puntos)
+    '''
     res = []
 
     with open(ruta_fichero, encoding="UTF-8") as f:
@@ -30,3 +38,26 @@ def lee_batallas(ruta_fichero: str) -> List[BatallaGOT]:
             res.append(BatallaGOT(nombre, rey_atacante, rey_atacado, gana_atacante, muertes_principales, comandantes_atacantes, comandantes_atacados, region, num_atacantes, num_atacados))
 
     return res 
+
+def reyes_mayor_menor_ejercito(batallas: List[BatallaGOT]) -> Tuple[str, str]:
+    '''
+    Recibe una lista de tuplas de tipo BatallaGOT y devuelve una tupla con dos cadenas
+    correspondientes a los nombres de los reyes con el mayor y el menor ejército,
+    respectivamente, del acumulado de todas las batallas. Para calcular el tamaño del
+    ejército de un rey se deben sumar los números de atacantes o de atacados de todas
+    las batallas en las que ha participado dicho rey como atacante o como atacado.
+    (2 puntos)
+    '''
+    batallas_reyes = defaultdict(int)
+
+    for batalla in batallas:
+        if batalla.num_atacantes != None:
+            batallas_reyes[batalla.rey_atacante] += batalla.num_atacantes
+        if batalla.num_atacados != None:
+            batallas_reyes[batalla.rey_atacado] += batalla.num_atacados
+
+    rey_mayor_ejercito = max(batallas_reyes, key = batallas_reyes.get)
+    rey_menor_ejercito = min(batallas_reyes, key = batallas_reyes.get)
+
+    
+    return (rey_mayor_ejercito, rey_menor_ejercito)
--- src/got_test.py
+++ src/got_test.py
@@ -7,6 +7,10 @@ def test_lee_batallas(batallas):
     print(f"\nCuarta batalla: {batallas[3]}")
     print(f"\nÚltima batalla: {batallas[-1]}")
 
+def test_reyes_mayor_menor_ejercito(reyes):
+    print(f"\n2. test_reyes_mayor_menor_ejercito\n{reyes}")
+
 if __name__ == "__main__":
     batallas = lee_batallas("data/battles.csv")
-    test_lee_batallas(batallas)
+    # test_lee_batallas(batallas)
+    test_reyes_mayor_menor_ejercito(reyes_mayor_menor_ejercito(batallas))
